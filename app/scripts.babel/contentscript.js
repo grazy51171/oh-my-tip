191eec12819d0e91187c30b43e65b4862fc084fc
Grazy <grazy51171@gmail.com>
2016-05-01 00:45:10 +0200

    Code format

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,126 +2,127 @@
 
 (function () {
 
-    var timerHandleTip = 0;
-    var tipQueue = [];
-    var tipConfig = [];
-    var otherOptions ={};
-    var isStarted = false;
+  var timerHandleTip = 0;
+  var tipQueue = [];
+  var tipConfig = [];
+  var otherOptions = {};
+  var isStarted = false;
 
-    //read configuration.
-    chrome.storage.onChanged.addListener(function (changes, namespace) {
-        var tipconfigChange = changes['tipConfig'];
+  //read configuration.
+  chrome.storage.onChanged.addListener(function (changes) {
+    var tipconfigChange = changes['tipConfig'];
 
-        if (tipconfigChange)
-            tipConfig = tipconfigChange.newValue;
+    if (tipconfigChange)
+      tipConfig = tipconfigChange.newValue;
+  });
+
+  chrome.storage.local.get(
+    {
+      tipConfig:
+      [
+        { tip: 1, action: { speed: 1, duration: 3 } },
+        { tip: 10, action: { speed: 2, duration: 10 } },
+        { tip: 100, action: { speed: 3, duration: 30 } },
+      ],
+      otherOptions: { randomLevelTip: 0 }
+    },
+    function (items) {
+      var tipConfigItem = items.tipConfig;
+      if (tipConfigItem) {
+        tipConfig = tipConfigItem;
+      }
+      otherOptions = items.otherOptions;
     });
 
-    chrome.storage.local.get(
-        {
-            tipConfig:
-            [
-                { tip: 1, action: { speed: 1, duration: 3 } },
-                { tip: 10, action: { speed: 2, duration: 10 } },
-                { tip: 100, action: { speed: 3, duration: 30 } },
-            ],
-            otherOptions: { randomLevelTip :0 }
-            }, function (items) {
-        var tipConfigItem = items.tipConfig;
-        if (tipConfigItem) {
-            tipConfig = tipConfigItem;
-        } 
-        otherOptions = items.otherOptions;
+
+  function changeSpeed(speed, user) {
+    chrome.runtime.sendMessage({ action: 'ChangeSpeed', speed: speed, user: user });
+  }
+
+  function getActionFromSpeed(amount) {
+    var action = { speed: 0, duration: 0 };
+    tipConfig.forEach(function (element) {
+      if (amount >= element.tip)
+        action = element.action;
+    }, this);
+
+    return action;
+  }
+
+  function dequeuOneTip() {
+    var message = tipQueue.shift();
+    if (message) {
+      // console.log('Handle tip from User ' + message.user + ' , ' + message.tip);
+      var action;
+      if (otherOptions.randomLevelTip == message.tip) {
+        action = tipConfig[Math.floor(Math.random() * tipConfig.length)].action;
+      }
+      else {
+        action = getActionFromSpeed(parseInt(message.tip));
+      }
+
+      if (action.speed > 0) {
+        changeSpeed(action.speed, message.user);
+        var textMessage = chrome.i18n.getMessage('reactMessage', [message.user, action.duration]);
+        sendMessage(textMessage);
+      }
+      timerHandleTip = setTimeout(dequeuOneTip, action.duration * 1000);
+    } else {
+      changeSpeed(0, '');
+      timerHandleTip = 0;
+    }
+  }
+
+  function tipsended(user, tokens) {
+    if (!isStarted)
+      return;
+    //        console.log("User "+user + " tip "+tokens);
+    var message = { user: user, tip: tokens };
+    tipQueue.push(message);
+    if (timerHandleTip === 0) {
+      dequeuOneTip();
+    }
+    // 
+  }
+
+  var searchTipRegex = /(.*)\s+tipped\s+(\d+)\s+token/;
+
+  // send message to the chat.
+  function sendMessage(texte) {
+    var lines = texte.split('\n');
+    $.each(lines, function (index, line) {
+      var messageJSON = JSON.stringify({ 'm': line, 'c': '', 'f': '' });
+      $('#movie')[0].SendRoomMsg(messageJSON);
     });
+  }
 
+  // Watch for tips.
+  setInterval(function () {
+    var tipMessages = $('.tipalert:gt(-5)');
 
-    function changeSpeed(speed, user) {
-        chrome.runtime.sendMessage({ action: 'ChangeSpeed', speed: speed, user: user });
-    }
-
-    function getActionFromSpeed(amount) {
-        var action = { speed:0, duration:0 };
-        tipConfig.forEach(function (element) {
-            if (amount >= element.tip)
-                action = element.action;
-        }, this);
-
-        return action;
-    }
-
-    function dequeuOneTip() {
-        var message = tipQueue.shift();
-        if (message) {
-            console.log('Handle tip from User ' + message.user + ' , ' + message.tip);
-            var action;
-            if(otherOptions.randomLevelTip == message.tip) {
-                action = tipConfig[Math.floor(Math.random() * tipConfig.length)].action;                
-            }
-            else {
-                action = getActionFromSpeed(parseInt(message.tip));    
-            }
-            
-            if(action.speed > 0) {
-                changeSpeed(action.speed, message.user);
-                var textMessage = chrome.i18n.getMessage('reactMessage', [message.user, action.duration]);
-                sendMessage(textMessage);                
-            }
-            timerHandleTip = setTimeout(dequeuOneTip, action.duration * 1000);
-        } else {
-            changeSpeed(0, '');
-            timerHandleTip = 0;
+    $.each(tipMessages,
+      function (index, element) {
+        var elementtext = $(element).text();
+        var match = searchTipRegex.exec(elementtext);
+        var isHandled = $(element).attr('gyIsHandled');
+        if (match && !isHandled) {
+          $(element).attr('gyIsHandled', '1');
+          tipsended(match[1], match[2]);
         }
-    }
+      });
+  }, 500);
 
-    function tipsended(user, tokens) {
-        if(!isStarted)
-            return;
-        //        console.log("User "+user + " tip "+tokens);
-        var message = { user: user, tip: tokens };
-        tipQueue.push(message);
-        if (timerHandleTip === 0) {
-            dequeuOneTip();
-        }
-        // 
-    }
-
-    var searchTipRegex = /(.*)\s+tipped\s+(\d+)\s+token/;
-    
-    // send message to the chat.
-    function sendMessage(texte) {
-        var lines = texte.split('\n');
-        $.each(lines, function (index, line) {
-            var messageJSON = JSON.stringify({ 'm': line, 'c': '', 'f': '' });
-            $('#movie')[0].SendRoomMsg(messageJSON);
-        });
-    }
-
-    // Watch for tips.
-    setInterval(function () {
-        var tipMessages = $('.tipalert:gt(-5)');
-
-        $.each(tipMessages,
-            function (index, element) {
-                var elementtext = $(element).text();
-                var match = searchTipRegex.exec(elementtext);
-                var isHandled = $(element).attr('gyIsHandled');
-                if (match && !isHandled) {
-                    $(element).attr('gyIsHandled', '1');
-                    tipsended(match[1], match[2]);
-                }
-            });
-    }, 500);
-    
-    chrome.runtime.onMessage.addListener(
+  chrome.runtime.onMessage.addListener(
     function (request, sender) {
-        if (request.action == 'ChangeState') {
-            isStarted = !isStarted;
-            chrome.runtime.sendMessage({ action: 'ChangeState', state:isStarted });
+      if (request.action == 'ChangeState') {
+        isStarted = !isStarted;
+        chrome.runtime.sendMessage({ action: 'ChangeState', state: isStarted });
 
-            var messageKey = isStarted ?  'activateExtension' : 'desactivateExtension';
-            var textMessage = chrome.i18n.getMessage(messageKey);
-            sendMessage(textMessage);                
-        }
+        var messageKey = isStarted ? 'activateExtension' : 'desactivateExtension';
+        var textMessage = chrome.i18n.getMessage(messageKey);
+        sendMessage(textMessage);
+      }
     });
-    
-    chrome.runtime.sendMessage({ action: 'ChangeState', state:isStarted });
+
+  chrome.runtime.sendMessage({ action: 'ChangeState', state: isStarted });
 })();
--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,86 +1,86 @@
 'use strict';
 
 function save_options() {
-    var tipConfig = [];
-    var otherOptions = {};
-    otherOptions.randomLevelTip = 0; 
-    $('#tipConfig tr').each(function () {
-        var tip = parseInt($(this).find('.tip').val());
-        var speed = parseInt($(this).find('.speed').val());
-        var duration = parseInt($(this).find('.duration').val());
-        if (tip && speed && duration)
-            tipConfig.push({ tip: tip, action: { speed: speed, duration: duration } });
-    });
+  var tipConfig = [];
+  var otherOptions = {};
+  otherOptions.randomLevelTip = 0;
+  $('#tipConfig tr').each(function () {
+    var tip = parseInt($(this).find('.tip').val());
+    var speed = parseInt($(this).find('.speed').val());
+    var duration = parseInt($(this).find('.duration').val());
+    if (tip && speed && duration)
+      tipConfig.push({ tip: tip, action: { speed: speed, duration: duration } });
+  });
 
-    otherOptions.randomLevelTip = parseInt($('#randomLevelTip').val());
-    
-    tipConfig = tipConfig.sort(function (a, b) {
-        if (a.tip < b.tip) return -1;
-        if (a.tip > b.tip) return 1;
-        return 0;
-    });
+  otherOptions.randomLevelTip = parseInt($('#randomLevelTip').val());
 
-    chrome.storage.local.set({
-        tipConfig: tipConfig, 
-        otherOptions : otherOptions,
-    }, function () {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-            restore_options();
-        }, 750);
-    });
+  tipConfig = tipConfig.sort(function (a, b) {
+    if (a.tip < b.tip) return -1;
+    if (a.tip > b.tip) return 1;
+    return 0;
+  });
+
+  chrome.storage.local.set({
+    tipConfig: tipConfig,
+    otherOptions: otherOptions,
+  }, function () {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function () {
+      status.textContent = '';
+      restore_options();
+    }, 750);
+  });
 }
 
 function addLine() {
-    $('#tipConfig').append('<tr>'
-        + '<td><input type=\'number\' min=\'0\' class=\'tip\' /></td>'
-        + '<td><input type=\'number\' min=\'0\' max=\'3\' class=\'speed\' /></td>'
-        + '<td><input type=\'number\' min=\'1\' class=\'duration\' /></td>'
-        + '<td><input type=\'button\' value=\'-\' class=\'removeLine\'></td>'
-        + '</tr>');
+  $('#tipConfig').append('<tr>'
+    + '<td><input type=\'number\' min=\'0\' class=\'tip\' /></td>'
+    + '<td><input type=\'number\' min=\'0\' max=\'3\' class=\'speed\' /></td>'
+    + '<td><input type=\'number\' min=\'1\' class=\'duration\' /></td>'
+    + '<td><input type=\'button\' value=\'-\' class=\'removeLine\'></td>'
+    + '</tr>');
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.local.get({
-        tipConfig:
-        [
-            { tip: 1, action: { speed: 1, duration: 3 } },
-            { tip: 10, action: { speed: 2, duration: 10 } },
-            { tip: 30, action: { speed: 3, duration: 30 } },
-        ],
-        otherOptions: { randomLevelTip :0 }
-    }, function (items) {
-        if (items.tipConfig) {
-            $('#tipConfig').empty();
-            items.tipConfig.forEach(function (element) {
-                addLine();
-                $('.tip').last().val(element.tip);
-                $('.speed').last().val(element.action.speed);
-                $('.duration').last().val(element.action.duration);
-            }, this);
-        }
-        if(items.otherOptions) {
-            $('#randomLevelTip').val(items.otherOptions.randomLevelTip);
-        }
-    });
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.local.get({
+    tipConfig:
+    [
+      { tip: 1, action: { speed: 1, duration: 3 } },
+      { tip: 10, action: { speed: 2, duration: 10 } },
+      { tip: 30, action: { speed: 3, duration: 30 } },
+    ],
+    otherOptions: { randomLevelTip: 0 }
+  }, function (items) {
+    if (items.tipConfig) {
+      $('#tipConfig').empty();
+      items.tipConfig.forEach(function (element) {
+        addLine();
+        $('.tip').last().val(element.tip);
+        $('.speed').last().val(element.action.speed);
+        $('.duration').last().val(element.action.duration);
+      }, this);
+    }
+    if (items.otherOptions) {
+      $('#randomLevelTip').val(items.otherOptions.randomLevelTip);
+    }
+  });
 }
 
 // remove line handling
-$('#tipConfig').on('click','.removeLine',
-    function () { $(this).closest('tr').remove(); }
-    );
- // add line handling
-$('#addline').on('click',addLine);
+$('#tipConfig').on('click', '.removeLine',
+  function () { $(this).closest('tr').remove(); }
+);
+// add line handling
+$('#addline').on('click', addLine);
 
-$('#save').on('click',save_options);
+$('#save').on('click', save_options);
 
 
-$(function() {
-    restore_options();
+$(function () {
+  restore_options();
 });
